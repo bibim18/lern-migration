7c40c428029803b7d94d9e2976b06a17761710b2
bibim18 <namfon_inuyasha@hotmail.com>
2017-12-25 18:17:12 +0700

    first

---
+++ models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    fname: DataTypes.STRING,
+    lname: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
